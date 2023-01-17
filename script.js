5436a427f2cec3493e5d6b44af7fecadb64f48ca
rodionion <rodionvh@gmail.com>
2023-01-17 16:01:19 +0100

    Add Merge Sort

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
 const input = document.querySelector("input");
-input.addEventListener("change", () => displayResult(sort(input.value)));
+input.addEventListener("change", () => displayResult(mergeSortCoordinator(input.value)));
 
-function sort(string) {
-    const array = string.split(",");
+function mergeSort(array) {
+
+    if (array.length > 1) {
+    
+        // Split array in 2
+        const middle = Math.round(array.length / 2);
+        let leftHalf = array.slice(0, middle);
+        let rightHalf = array.slice(middle);
+
+        // MergeSort both halves
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
+    
+        // Merge
+        let i = 0;
+        let j = 0;
+        let k = 0;
+    
+        while ((i < leftHalf.length) && (j < rightHalf.length)) {
+    
+            if (leftHalf[i] < rightHalf[j]) {
+                array[k] = leftHalf[i];
+                i++;
+            } else {
+                array[k] = rightHalf[j];
+                j++;
+            }
+            k++;
+        };
+
+        // Collect remaining odd parts
+        while (i < leftHalf.length) {
+            array[k] = leftHalf[i];
+            i++;
+            k++;
+        }
+        while (j < rightHalf.length) {
+            array[k] = rightHalf[j];
+            j++;
+            k++;
+        }
+    }
+}
+
+function mergeSortCoordinator (array) {
+    if (typeof array === "string") {
+        array = array.split(",");
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        array[i] = Number(array[i]);
+    }
+
+    mergeSort(array);
+
     return array;
 }
 
@@ -11,4 +64,4 @@ function displayResult(result) {
     resultParagraph.innerText = result;
 }
 
-displayResult(sort(input.placeholder));
+displayResult(mergeSortCoordinator(input.placeholder));
